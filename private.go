061682abbc44e6eef500672bd9507b316de940c1
hnrq104 <henriquelc@dcc.ufrj.br>
2024-03-07 23:57:57 -0300

    heap already tested

---
+++ private.go
@@ -0,0 +1,84 @@
+package fibonacci
+
+// adds the node x to the left of it's destination
+func addNodeToCircularList(x, dst *FibonnaciNode) {
+	//keep structure of underlying list
+	x.left.right = x.right
+	x.right.left = x.left
+
+	x.right = dst
+	x.left = dst.left
+
+	x.right.left = x
+	x.left.right = x
+}
+
+// Node links subjugates a node to another, returnin the one with lesser key
+func nodeLink(x, y *FibonnaciNode) *FibonnaciNode {
+	if x == nil || y == nil {
+		panic("nodeLink called with nil arguments")
+	}
+	if y.key < x.key {
+		x, y = y, x
+	}
+
+	y.left.right = y.right
+	y.right.left = y.left
+
+	if x.child != nil {
+		y.right = x.child
+		y.left = x.child.left
+	} else {
+		x.child = y
+	}
+
+	y.left.right = y
+	y.right.left = y
+
+	x.degree++
+	y.mark = false
+
+	return x
+}
+
+func (h *FibonnaciHeap) cut(x *FibonnaciNode) {
+	if x.parent.child == x {
+		x.parent.child = x.right
+	}
+	x.parent = nil
+	addNodeToCircularList(x, h.min)
+	x.mark = false
+}
+
+func (h *FibonnaciHeap) cascadingcut(x *FibonnaciNode) {
+	z := x.parent
+	if z == nil {
+		return
+	}
+	if !x.mark {
+		x.mark = true
+	} else {
+		h.cut(x)
+		h.cascadingcut(z)
+	}
+}
+
+func (h *FibonnaciHeap) lengthAndMaxDegree() (int, int) {
+	if h.min == nil {
+		return 0, 0
+	}
+
+	var length, maxdegree int
+
+	aux := h.min
+	for {
+		length++
+		maxdegree = max(maxdegree, aux.degree)
+		if aux.right == h.min {
+			break
+		}
+		aux = aux.right
+	}
+
+	return length, maxdegree
+}
---
+++ tree.go
@@ -0,0 +1,181 @@
+package fibonacci
+
+// Start with float64, then we see whatelse
+type FibonnaciHeap struct {
+	size  int
+	roots int
+	min   *FibonnaciNode
+}
+
+type FibonnaciNode struct {
+	key    float64        // Key in the node
+	degree int            // Number of children
+	mark   bool           // Checks if a child was removed
+	parent *FibonnaciNode // Parent of the node
+	right  *FibonnaciNode // Sibling node to the right
+	left   *FibonnaciNode // Sibling node to the left
+	child  *FibonnaciNode // First child in circular linked list
+}
+
+func (h *FibonnaciHeap) Size() int {
+	return h.size
+}
+
+func (x *FibonnaciNode) Key() float64 {
+	return x.key
+}
+
+func NewFibonnaciHeap() FibonnaciHeap {
+	return FibonnaciHeap{size: 0, min: nil, roots: 0}
+}
+
+func NewFibonnaciNode(k float64) *FibonnaciNode {
+	n := &FibonnaciNode{key: k}
+	n.left, n.right = n, n
+	return n
+}
+
+// Inserts it as a new Tree in the Root List!
+func (heap *FibonnaciHeap) InsertNode(x *FibonnaciNode) {
+	x.degree = 0
+	x.child = nil
+	x.parent = nil
+	x.mark = false
+
+	if heap.min == nil {
+		heap.min = x
+	} else {
+		x.right = heap.min
+		x.left = heap.min.left
+		x.left.right = x
+		x.right.left = x
+
+		if x.key < heap.min.key {
+			heap.min = x
+		}
+	}
+	heap.size++
+	heap.roots++
+}
+
+func FibonnaciUnion(lhsHeap, rhsHeap FibonnaciHeap) FibonnaciHeap {
+	if lhsHeap.min == nil {
+		return rhsHeap
+	}
+	if rhsHeap.min == nil {
+		return lhsHeap
+	}
+
+	heap := NewFibonnaciHeap()
+	//concatenate both
+	h1, h2 := lhsHeap.min, rhsHeap.min
+	h1.left.right = h2.right
+	h2.right.left = h1.left
+	h2.right = h1
+	h1.left = h2
+
+	heap.min = h1
+	if h2.key < heap.min.key {
+		heap.min = h2
+	}
+	heap.size = lhsHeap.size + rhsHeap.size
+	heap.roots = lhsHeap.roots + rhsHeap.roots
+	return heap
+}
+
+func (heap *FibonnaciHeap) ExtractMin() *FibonnaciNode {
+	removed := heap.min
+	if removed == nil {
+		return nil
+	}
+
+	nodeptr := removed.child
+	// Worst do-while in my life
+	if nodeptr != nil {
+		for {
+			aux := nodeptr
+			nodeptr = nodeptr.right
+			addNodeToCircularList(aux, heap.min)
+			aux.parent = nil
+
+			if aux == nodeptr {
+				break
+			}
+
+		}
+	}
+
+	//remove from root list
+	removed.left.right = removed.right
+	removed.right.left = removed.left
+
+	if removed.right == removed {
+		heap.min = nil
+	} else {
+		heap.min = removed.right
+		heap.consolidate()
+	}
+
+	heap.size--
+	heap.roots += removed.degree - 1
+	return removed
+}
+
+func (h *FibonnaciHeap) consolidate() {
+	rootlength, _ := h.lengthAndMaxDegree()
+	newroots := make([]*FibonnaciNode, rootlength+1)
+	aux := h.min
+	for i := 0; i < rootlength; i++ {
+		d := aux.degree
+		for newroots[d] != nil {
+			aux = nodeLink(aux, newroots[d])
+			newroots[d] = nil
+			d++
+		}
+		newroots[d] = aux
+		if aux == aux.right {
+			break
+		}
+
+		//remove aux from list
+		aux.left.right = aux.right
+		aux.right.left = aux.left
+
+		aux = aux.right
+	}
+
+	for i := 0; i < len(newroots); i++ {
+		if newroots[i] == nil {
+			continue
+		}
+
+		if h.min == nil {
+			h.min = newroots[i]
+			h.min.left = h.min
+			h.min.right = h.min
+		} else {
+			addNodeToCircularList(newroots[i], h.min)
+			if newroots[i].key < h.min.key {
+				h.min = newroots[i]
+			}
+		}
+	}
+
+}
+
+func (h *FibonnaciHeap) DecreaseKey(x *FibonnaciNode, k float64) {
+	if k > x.key {
+		panic("Increasing Key value")
+	}
+	x.key = k
+	y := x.parent
+
+	if y != nil && x.key < y.key {
+		h.cut(x)
+		h.cascadingcut(y)
+	}
+
+	if h.min.key < x.key {
+		h.min = x
+	}
+}
